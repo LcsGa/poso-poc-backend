2197016031aefdf08ceaf51accfbddc8f422f5c4
lgarcia <lucas.garcia@equasens.com>
2023-04-28 14:55:05 +0200

    get the print info command

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,16 @@
-import { findByIds, OutEndpoint } from "usb";
-import Encoder from "@manhnd/esc-pos-encoder";
+import { readFile } from "fs";
 
-const device = findByIds(0x4f9, 0x2057);
-
-if (!device) {
-  console.error("Imprimante non trouvée");
-  process.exit(1);
-}
-
-device.open();
-
-const printerInterface = device.interface(0);
-if (printerInterface.isKernelDriverActive()) {
-  try {
-    printerInterface.detachKernelDriver();
-  } catch (error) {
-    console.error("Impossible de détacher le pilote du noyau :", error);
-    process.exit(1);
-  }
-}
-
-printerInterface.claim();
-
-const endpoint = printerInterface.endpoints[1] as OutEndpoint;
-
-const buffer = Buffer.from(new Encoder().text("The quick brown fox jumps over the lazy dog").encode());
-
-endpoint.transfer(buffer, (error) => {
-  if (error) {
-    console.error("Erreur lors de l'envoi des données à l'imprimante :", error);
-  } else {
-    console.log("Impression réussie");
-  }
-
-  printerInterface.release(true, (error) => {
-    if (error) {
-      console.error("Erreur lors de la libération de l'interface :", error);
-    }
-    device.close();
-  });
-});
+readFile("C:/Users/lgarcia/Desktop/test.bin", (_, data) =>
+  console.log(
+    "{ " +
+      [...data]
+        .map((intByte) => intByte.toString(16).padStart(2, "0"))
+        .join(" ")
+        .replace(/.*(1b 69 55 77 01)/i, "$1")
+        .split(" ")
+        .slice(0, 5 + 127)
+        .map((hexByte) => "0x" + hexByte)
+        .join(", ") +
+      " }"
+  )
+);
